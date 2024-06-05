9b7cd0b4e59e877bbe61d9ac5a299aa635c82354
Daria <d.makarova@ftr.group>
2024-06-05 16:31:10 -0400

    167. Two Sum II - Input Array Is Sorted

---
+++ src/main/java/org/example/solutions/Two_Pointers/TwoIntegerSumII.java
@@ -0,0 +1,25 @@
+package org.example.solutions.Two_Pointers;
+
+import java.util.ArrayList;
+
+public class TwoIntegerSumII {
+    public int[] solution(int[] numbers, int target) {
+        int firstIndex = 0;
+        int secondIndex = numbers.length - 1;
+        while (firstIndex != secondIndex){
+            if (numbers[firstIndex] + numbers[secondIndex] == target) {
+                return new int[]{++firstIndex, ++secondIndex};
+            }
+
+            if (numbers[firstIndex] + numbers[secondIndex] > target) {
+                --secondIndex;
+                continue;
+            }
+
+            if (numbers[firstIndex] + numbers[secondIndex] < target) {
+                ++firstIndex;
+            }
+        }
+        return new int[]{};
+    }
+}
