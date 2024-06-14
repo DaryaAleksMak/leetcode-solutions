f02b30e1074e3c4b64b85f4a9c3fed89d78102c0
Daria <d.makarova@ftr.group>
2024-06-14 11:13:09 -0400

    503. Next Greater Element II

---
+++ src/main/java/org/example/solutions/stack/NextGreaterElementII.java
@@ -0,0 +1,49 @@
+package org.example.solutions.stack;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class NextGreaterElementII {
+    public int[] nextGreaterElements(int[] nums) {
+
+        int[] mergedArray = new int[nums.length * 2];
+        System.arraycopy(nums, 0, mergedArray, 0, nums.length);
+        System.arraycopy(nums, 0, mergedArray, nums.length, nums.length);
+
+        int[] result = new int[nums.length * 2];
+
+        var stack = new Stack<Integer>();
+        for (int i = mergedArray.length - 1; i >= 0; i--) {
+            var element = mergedArray[i];
+            if (stack.isEmpty()) {
+                stack.push(element);
+                result[i] = -1;
+                continue;
+            }
+
+            if (element < stack.peek()) {
+                result[i] = stack.peek();
+                stack.push(element);
+                continue;
+            }
+
+            while (true) {
+                if (stack.isEmpty()) {
+                    stack.push(element);
+                    result[i] = -1;
+                    break;
+                }
+
+                if (element >= stack.peek()) {
+                    stack.pop();
+                } else {
+                    result[i] = stack.peek();
+                    stack.push(element);
+                    break;
+                }
+            }
+        }
+
+        return Arrays.copyOfRange(result, 0, nums.length);
+    }
+}
