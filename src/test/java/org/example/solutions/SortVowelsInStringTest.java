634e82b193623e213a577271373427ca51da9771
Daria <darya@eucalyptuslabs.com>
2025-01-20 16:48:57 -0500

    SortVowelsInString

---
+++ src/test/java/org/example/solutions/SortVowelsInStringTest.java
@@ -0,0 +1,26 @@
+package org.example.solutions;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class SortVowelsInStringTest {
+
+  @ParameterizedTest
+  @MethodSource("testCases")
+  void testSolution(String input, String expectedResult) {
+    var task = new SortVowelsInString();
+    String result = task.sortVowels(input);
+    Assertions.assertEquals(expectedResult, result);
+  }
+
+  public static Stream<Arguments> testCases() {
+    return Stream.of(
+        Arguments.of("lEetcOde", "lEOtcede"),
+        Arguments.of("lYmpH", "lYmpH")
+    );
+  }
+
+}
