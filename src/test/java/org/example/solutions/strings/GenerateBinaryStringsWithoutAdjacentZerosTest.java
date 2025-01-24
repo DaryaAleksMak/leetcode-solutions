821a5ba2eb5cfe7fef495f7c93d87ee360227bd4
Daria <darya@eucalyptuslabs.com>
2025-01-24 14:18:00 -0500

    GenerateBinaryStringsWithoutAdjacentZeros

---
+++ src/test/java/org/example/solutions/strings/GenerateBinaryStringsWithoutAdjacentZerosTest.java
@@ -0,0 +1,31 @@
+package org.example.solutions.strings;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+import org.example.solutions.Two_Pointers.ThreeIntegerSum;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class GenerateBinaryStringsWithoutAdjacentZerosTest {
+
+  @ParameterizedTest
+  @MethodSource("twoSumTestCases")
+  void testSolution(int n, List<String> expectedResult) {
+    var  task = new GenerateBinaryStringsWithoutAdjacentZeros();
+    List<String> result = task.validStrings(n);
+    Assertions.assertNotNull(result);
+//        Assertions.assertArrayEquals(expectedResult, result);
+  }
+
+  public static Stream<Arguments> twoSumTestCases() {
+    return Stream.of(
+        Arguments.of(3, List.of("010","011","101","110","111")),
+        Arguments.of(1, List.of("0","1")));
+  }
+
+}
