6193819b58d235dfd7d3ef56122c683996f28b0c
Daria <darya.aleks.mak@gmail.com>
2025-02-18 18:36:42 -0500

    Sort The People

---
+++ src/main/java/org/example/solutions/sorting/SortThePeople.java
@@ -0,0 +1,25 @@
+package org.example.solutions.sorting;
+
+import java.util.Collections;
+import java.util.TreeMap;
+
+public class SortThePeople {
+
+  public String[] sortPeople(String[] names, int[] heights) {
+    var heightToNameMap = new TreeMap<Integer, String>(Collections.reverseOrder());
+
+    for (int i = 0; i < names.length; i++) {
+      heightToNameMap.put(heights[i], names[i]);
+    }
+
+    var result = new String[names.length];
+
+    int i = 0;
+    for (String name : heightToNameMap.values()) {
+      result[i++] = name;
+    }
+
+    return result;
+  }
+
+}
