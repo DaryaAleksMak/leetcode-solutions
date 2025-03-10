17bd5c9d3d1d1c5c4be1a23d1540553131de3ac6
Daria <darya.aleks.mak@gmail.com>
2025-03-10 12:57:13 -0400

    add description for problem solution

--- src/main/java/org/leetcode/solutions/Arrays_Hashing/EncodeAndDecodeStrings.java
+++ src/main/java/org/leetcode/solutions/Arrays_Hashing/EncodeAndDecodeStrings.java
@@ -3,6 +3,21 @@ package org.leetcode.solutions.Arrays_Hashing;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * {"hello", "word", "!"}
+ * Flow:
+ * init some delimiter for encoding and decoding
+ *
+ * encode:
+ * add at the start of each word length of it + delimiter
+ *  result of encoding: "5#hello4#1#!"
+ * decoding:
+ * 1. string -> charArray: "5#hello4#1#!" ->  ['5','#','h','e','l','l','o','4','#','1','#','!']
+ * 2. read char array while digit is it length of next word  = currentWordLength
+ * 3. read next currentWordLength chars with skipping delimiter
+ * 4. build word with StringBuilder and add it to result list
+ * 5. repeat it while we don't read all chars!
+ */
 public class EncodeAndDecodeStrings {
 
   private final static String delimeter = "@";
